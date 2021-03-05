5fab365f1f6182743fc8c27c7f9ca7df6ab2c290
Will Marttala <wmarttala123@gmail.com>
2021-03-05 13:24:17 -0600

    Initial commit

---
+++ fibonacci-and-fizzbuzz/src/main/java/com/cooksys/ftd/assignments/control/FizzBuzz.java
@@ -0,0 +1,127 @@
+package com.cooksys.ftd.assignments.control;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * FizzBuzz is an old programming exercise.
+ * The goal is to iterate over a range of numbers and print a message about each number's divisibility.
+ * <p>
+ * The message is generated the following way:
+ * *) if the number is divisible by three, the message is `Fizz`
+ * *) if the number is divisible by five, the message is `Buzz`
+ * *) if the number is divisible by both three and five, the message is `FizzBuzz`
+ * *) otherwise, no message is produced
+ * <p>
+ * The exact message format for this assignment is specified in the `message(n)` method.
+ */
+public class FizzBuzz {
+
+    /**
+     * Checks whether a given int `a` is evenly divisible by a given int `b` or not.
+     * For example, `divides(4, 2)` returns `true` and `divides(4, 3)` returns `false`.
+     *
+     * @param a the number to be divided
+     * @param b the number to divide by
+     * @return `true` if a is evenly divisible by b, `false` otherwise
+     * @throws IllegalArgumentException if b is zero
+     */
+    public static boolean divides(int a, int b) throws IllegalArgumentException {
+        if (b == 0) {
+        	throw new IllegalArgumentException("The provided b cannot by 0. Cannot divide by 0.");
+        }
+        return a % b == 0;
+    }
+
+    /**
+     * Generates a divisibility message for a given number. Returns null if the given number is not divisible by 3 or 5.
+     * Message formatting examples:
+     * 1 -> null // not divisible by 3 or 5
+     * 3 -> "3: Fizz" // divisible by only 3
+     * 5 -> "5: Buzz" // divisible by only 5
+     * 7 -> "7: Bear" // divisible by only 7
+     * 15 -> "15: FizzBuzz" // divisible by both three and five
+     * 21 -> "21: Fizz"
+     *
+     * @param n the number to generate a message for
+     * @return a message according to the format above, or null if n is not divisible by either 3 or 5
+     */
+    public static String message(int n) {
+        String result = n + ": ";
+        if (divides(n, 3)) {
+        	result += "Fizz";
+        }
+        if (divides(n, 5)) {
+        	result += "Buzz";
+        }
+        return result.equals(n + ": ") ? null : result;
+    }
+        
+
+    /**
+     * Generates an array of messages to print for a given range of numbers.
+     * If there is no message for an individual number (i.e., `message(n)` returns null),
+     * it should be excluded from the resulting array.
+     *
+     * @param start the number to start with (inclusive)
+     * @param end the number to end with (exclusive)
+     * @return an array of divisibility messages
+     * @throws IllegalArgumentException if the given end is less than the given start
+     */
+    public static String[] messages(int start, int end) throws IllegalArgumentException {
+    	if (end < start) {
+    		throw new IllegalArgumentException("The provided end value must be greater than the provided start value.");
+    	}
+    	
+//    	// Figure out how many messages are in the provided range
+//    	int resultSize = 0;
+//    	for (int i = start; i < end; i++) {
+//    		if (message(i) != null) {
+//    			resultSize++;
+//    		}
+//    	}
+//    	
+//    	// create our result array of messages and populate it
+//        String[] result = new String[resultSize];
+//        int currIdx = 0;
+//        for (int i = start; i < end; i++) {
+//        	if (message(i) != null) {
+//        		result[currIdx] = message(i);
+//        		currIdx++;
+//        	}
+//        }
+    	
+//    	String[] result = new String[0];
+//    	for (int i = start; i < end; i++) {
+//    		String msg = message(i);
+//    		if (msg != null) {
+//    			String[] temp = Arrays.copyOf(result, result.length + 1);
+//    			temp[temp.length - 1] = msg;
+//    			result = temp;
+//    		}
+//    	}
+    	
+    	List<String> result = new ArrayList<>();
+    	for (int i = start; i < end; i++) {
+    		String msg = message(i);
+    		if (msg != null) {
+    			result.add(msg);
+    		}
+    	}
+        return result.toArray(new String[result.size()]);
+    }
+
+    /**
+     * For this main method, iterate over the numbers 1 through 115 and print
+     * the relevant messages to sysout
+     */
+    public static void main(String[] args) {
+    	for (String message : messages(1, 116)) {
+    		System.out.println(message);
+    	}
+    }
+
+}
